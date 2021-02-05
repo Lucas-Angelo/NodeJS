216431b68e77146797de149b1ed2db707c6e7d61
lcs2001 <lcs2001_@hotmail.com>
2021-02-05 09:37:40 -0300

    Import e export modulos

---
+++ inicio/app.js
@@ -0,0 +1,15 @@
+const calcular = require("./calculos");
+
+console.log(calcular.soma(5, 6));
+console.log(calcular.subtrai(5, 6));
+
+/*
+var printar = true;
+const comando = "node app.js";
+
+if(printar) {
+    console.log("Hello world!");
+    console.log("Entrar em /inicio/");
+    console.log(comando);
+}
+*/
